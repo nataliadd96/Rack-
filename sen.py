539d81bfb2e41cbfc01cc893bc00f9f395a1e7c3
juchani <victorfrancojuchani@gmail.com>
2020-03-13 17:57:52 -0400

    13-03

---
+++ sen.py
@@ -0,0 +1,49 @@
+from w1thermsensor import W1ThermSensor as ds 
+from gpiozero import LED
+from time import sleep as t
+from influxdb import InfluxDBClient as influx
+cliente = influx(database='rack')
+
+def ds18b20():
+    try:
+        s1 = ds(ds.THERM_SENSOR_DS18B20,"030897943b31")
+        s2 = ds(ds.THERM_SENSOR_DS18B20,"03ff979418c9")
+        s3 = ds(ds.THERM_SENSOR_DS18B20,"0417c45487ff")
+        t3 = float(s1.get_temperature())
+        t2 = float(s2.get_temperature())
+        t1 = float(s3.get_temperature())
+        data1=[]
+        data2=[]
+        data3=[]
+        data1.append("ds18b20,tag=1 temp={}".format(t1))
+        cliente.write_points(data1, database='rack', time_precision='s', protocol='line')
+        data2.append("ds18b20,tag=2 temp={}".format(t2))
+        cliente.write_points(data2, database='rack', time_precision='s', protocol='line') 
+        data3.append("ds18b20,tag=3 temp={}".format(t3))
+        cliente.write_points(data3, database='rack', time_precision='s', protocol='line')
+        print("sensor1 {} C  sensor2 {} C sensor3 {} C".format(t1,t2,t3))
+    except :
+        data1=[]
+        data2=[]
+        data3=[]
+        data1.append("ds18b20,tag=1 temp={}".format("35"))
+        cliente.write_points(data1, database='rack', time_precision='s', protocol='line')
+        data2.append("ds18b20,tag=2 temp={}".format("35"))
+        cliente.write_points(data2, database='rack', time_precision='s', protocol='line') 
+        data3.append("ds18b20,tag=3 temp={}".format("35"))
+        cliente.write_points(data3, database='rack', time_precision='s', protocol='line')
+        print("sensor1 {} C  sensor2 {} C sensor3 {} C".format(35,35,35))
+        print ("error de sensor ds18b20") 
+led= LED(12)
+
+
+led.off()
+t(1)
+led.on()
+t(1)
+while 1:
+    ds18b20()
+    t(0.7)
+    
+   
+    
